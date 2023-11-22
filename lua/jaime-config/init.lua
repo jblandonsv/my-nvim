41a63ee128cd0108179ffa350a33c49d7ff7b6bc
Jaime Blandon <jaime.blandon@invitationhomes.com>
2023-11-22 14:52:02 -0600

    prettier

--- lua/jaime-config/init.lua
+++ lua/jaime-config/init.lua
@@ -14,4 +14,7 @@ require("jaime-config.crates")
 
 require("jaime-config.apex-config")
 
+require("jaime-config.null-ls")
+require("jaime-config.prettier")
+
 require("dap-python").setup("/Users/jaime/debug/bin/python")
