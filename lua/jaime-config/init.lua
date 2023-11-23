3e4ececb936be20183391da81943b9cf2ebfdde9
Jaime Blandon <jaime.blandon@invitationhomes.com>
2023-11-23 09:08:03 -0600

    usando conform como formatting tool

--- lua/jaime-config/init.lua
+++ lua/jaime-config/init.lua
@@ -7,14 +7,11 @@ require("jaime-config.theme")
 require("jaime-config.telescope")
 
 require("jaime-config.python-config")
-require("jaime-config.python-ruff-config")
+-- require("jaime-config.python-ruff-config")
 require("jaime-config.rust-tools")
 require("jaime-config.lualine")
 require("jaime-config.crates")
 
 require("jaime-config.apex-config")
-
-require("jaime-config.null-ls")
-require("jaime-config.prettier")
 
 require("dap-python").setup("/Users/jaime/debug/bin/python")
--- lua/plugins/prettier.lua
+++
@@ -1,7 +0,0 @@
-return {
-    'MunifTanjim/prettier.nvim',
-    dependencies = {
-        {'neovim/nvim-lspconfig'},
-        {'jose-elias-alvarez/null-ls.nvim'},
-    }
-}
