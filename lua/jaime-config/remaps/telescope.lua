dfc10f50983a9d6e0a4ee039129f736cf51227dd
Jaime Blandon <jaime.blandon@invitationhomes.com>
2024-03-12 09:57:47 -0600

    keymap para Telescope fuzzy find en el current buf

--- lua/jaime-config/remaps/telescope.lua
+++ lua/jaime-config/remaps/telescope.lua
@@ -4,6 +4,7 @@ vim.keymap.set('n', '<leader>ff', ":Telescope find_files hidden=true<cr>")
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>fc', builtin.current_buffer_fuzzy_find, {})
 vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ")});
+    builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
