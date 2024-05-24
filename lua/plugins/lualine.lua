0da16e3b6c7d2dfb88f212d391acff6fc7988bd6
Jaime Blandon <jaime.blandon@invitationhomes.com>
2024-05-23 18:40:38 -0600

    onedark + lualine relative path

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,4 +1,4 @@
 return {
-  'nvim-lualine/lualine.nvim',
-  requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    'nvim-lualine/lualine.nvim',
+    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
 }
